fb1202e67800389c2af2e2af9b326b79bdbbdbeb
Andreas <ahindman@uw.edu>
2017-12-02 11:49:02 -0800

    Added comments and labeled bar graph output.

--- server.R
+++ server.R
@@ -1,15 +1,14 @@
 library(shiny)
-
 library(ggplot2)
 
+# Variables
 survey <- read.csv("data/survey.csv")
 
 server <- function(input, output){
   output$bar <- renderPlot({
     xColumns <- input$xInput
-    
     # Plot graph
-    ggplot(survey, aes(survey[,xColumns])) + geom_bar(aes(fill = mental_health_consequence), position = "stack")
+    ggplot(survey, aes(survey[,xColumns])) + geom_bar(aes(fill = mental_health_consequence), position = "stack") + xlab(input$xInput) + ggtitle(paste(input$xInput, "in survey dataset")) + labs(fill="Mental Health Consequences?")
     
   })
 }
--- ui.R
+++ ui.R
@@ -1,16 +1,20 @@
 library(shiny)
-
 library(readr)
 
+# Variables 
 survey <- read.csv("data/survey.csv")
 introText <- read_file("intro.txt")
 
+# UI
 fluidPage(
   h1("Mental Health in the Tech Industry"), p("DataFriends"),
   hr(),
   
   tabsetPanel(
+    # Introduction tab. Reads text from 'intro.txt'.
     tabPanel("Introduction", h2("Introduction"), p(introText)),
+    
+    # Displays 
     tabPanel("Bar Graphs", sidebarLayout(
                               # Input
                               sidebarPanel(selectInput("xInput", label = "Choose a variable to display on the x axis.", choices = names(survey), selected = 'remote_work')),
@@ -18,6 +22,8 @@ fluidPage(
                               mainPanel(plotOutput("bar"))
                           )
     ),
+    
+    # Sentiment Analysis
     tabPanel("Sentiment Analysis", "contents")),
   
   # GitHub link
