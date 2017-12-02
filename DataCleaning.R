e961125a524addc12606e9d2df4358bb947e22fc
Javad Goudarzi <jgoud@uw.edu>
2017-12-02 11:29:17 -0800

    changed

--- DataCleaning.R
+++ DataCleaning.R
@@ -11,13 +11,13 @@ print(age)
 View(age)
 #plot 1: x=age, y=supervisor
 
-plot1 <- ggplot(mental_health_data, aes(x=mental_health_data$Age, y=mental_health_data$supervisor)) + geom_col() + coord_flip() + scale_colour_gradient2(low = "blue", mid = "green", high = "red")
+plot1 <- ggplot(mental_health_data, aes(x=mental_health_data$Age, y=mental_health_data$supervisor)) + geom_bar() + coord_flip() + scale_colour_gradient2(low = "blue", mid = "green", high = "red")
 print(plot1)
 
-plot2 <- ggplot(mental_health_data, aes(x=mental_health_data$Gender, y=mental_health_data$supervisor)) + geom_col() + coord_flip() + scale_colour_gradient2(low = "blue", mid = "green", high = "red")
+plot2 <- ggplot(mental_health_data, aes(x=mental_health_data$Gender, y=mental_health_data$supervisor)) + geom_bar() + coord_flip() + scale_colour_gradient2(low = "blue", mid = "green", high = "red")
 print(plot2)
 
-plot3 <- ggplot(mental_health_data, aes(x=mental_health_data$state, y=mental_health_data$supervisor)) + geom_col() + coord_flip() + scale_colour_gradient2(low = "blue", mid = "green", high = "red")
+plot3 <- ggplot(mental_health_data, aes(x=mental_health_data$state, y=mental_health_data$supervisor)) + geom_bar() + coord_flip() + scale_colour_gradient2(low = "blue", mid = "green", high = "red")
 print(plot3)
 
 
