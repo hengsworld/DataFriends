af7b1cf72f5f6d071392fb690c0e29c2d5a6ac9d
Andreas <ahindman@uw.edu>
2017-12-02 11:17:18 -0800

    Formatted ui, added bar graphs, started introduction.

---
+++ server.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(ggplot2)
+
+survey <- read.csv("data/survey.csv")
+
+server <- function(input, output){
+  output$bar <- renderPlot({
+    xColumns <- input$xInput
+    
+    # Plot graph
+    ggplot(survey, aes(survey[,xColumns])) + geom_bar(aes(fill = mental_health_consequence), position = "stack")
+    
+  })
+}
---
+++ ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(readr)
+
+survey <- read.csv("data/survey.csv")
+introText <- read_file("intro.txt")
+
+fluidPage(
+  h1("Mental Health in the Tech Industry"), p("DataFriends"),
+  hr(),
+  
+  tabsetPanel(
+    tabPanel("Introduction", h2("Introduction"), p(introText)),
+    tabPanel("Bar Graphs", sidebarLayout(
+                              # Input
+                              sidebarPanel(selectInput("xInput", label = "Choose a variable to display on the x axis.", choices = names(survey), selected = 'remote_work')),
+                              # Output
+                              mainPanel(plotOutput("bar"))
+                          )
+    ),
+    tabPanel("Sentiment Analysis", "contents")),
+  
+  # GitHub link
+  a(href="https://github.com/hengsworld/DataFriends", "GitHub")
+)
+
+
